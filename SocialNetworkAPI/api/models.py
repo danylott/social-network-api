3f6e8940432f6d476988252e554fdcb8401a4238
Daniel <d.tuytuyshkin@gmail.com>
2020-04-28 19:54:39 +0300

    wrote first model

--- SocialNetworkAPI/api/models.py
+++ SocialNetworkAPI/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+
